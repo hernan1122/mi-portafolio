65ec6c17c17c44b3c7dbf34bd3a2b563a517a4ee
Axel Torrico <torricohernan2@gmail.com>
2022-09-23 15:20:20 -0300

    cambie algunos estilos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const menuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
+const about = document.querySelector('#about');
+const jobs = document.querySelector('#jobs');
+const contacts = document.querySelector('#contacts');
 
 menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
+  const isAboutClosed = about.classList.contains('inactive');
+  if (!isAboutClosed) {
+    about.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
 }
