9ff050d8962a703b89306165996727951351d97c
Axel Torrico <torricohernan2@gmail.com>
2022-09-28 10:50:12 -0300

    agregue button-up

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const about = document.querySelector('#about');
 const jobs = document.querySelector('#jobs');
 const contacts = document.querySelector('#contacts');
-const barsMenu = document.querySelector(".bars__menu")
+const barsMenu = document.querySelector('.bars__menu');
+const buttonUp = document.querySelector('#button-up');
 
 mobileMenu.addEventListener('click', animateBars);
-barsMenu.addEventListener("click", animateBars);
+barsMenu.addEventListener('click', animateBars);
+buttonUp.addEventListener('click', scrollUp);
 
 let line1__bars = document.querySelector(".line1__bars-menu");
 let line2__bars = document.querySelector(".line2__bars-menu");
 let line3__bars = document.querySelector(".line3__bars-menu");
 
-function animateBars(){
+function animateBars() {
     // aplica a los nodos seleccionados el estilo de css
   line1__bars.classList.toggle("activeline1__bars-menu");
   line2__bars.classList.toggle("activeline2__bars-menu");
@@ -22,4 +24,27 @@ function animateBars(){
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle('inactive');
+}
+
+function scrollUp() {
+  // esta var nos indica que es el scroll de la pag
+  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+  // si currentScroll es mayor a 0
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(scrollUp);
+    window.scrollTo(0, currentScroll - (currentScroll / 10));
+  }
+}
+
+// para que aparezca o desaparezca el boton segun el scroll
+window.onscroll = function () {
+  let scroll = document.documentElement.scrollTop;
+
+  // si scroll es mayor a 100
+  if (scroll > 400) {
+    buttonUp.style.transform = 'scale(1)';
+  } else if (scroll < 200) {
+    buttonUp.style.transform = 'scale(0)';
+  }
 }
